b5dd99a10c0e7873b1f2b1d4bf0aed710e9584c8
unknown <har0216@tlu.ee>
2021-04-06 21:11:55 +0300

    Kodutoo

--- w07_junit/Poomine/hangman/src/main/java/org/hangman/Hangman.java
+++ w07_junit/Poomine/hangman/src/main/java/org/hangman/Hangman.java
@@ -1,13 +1,35 @@
 package org.hangman;
 
+import java.util.Scanner;
+import java.util.Random;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class Hangman
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static int wrongGuess=0;
+    private static String[] words = {"apple", "banana", "programming", "thunderstorm", "Terminator", "Slicer","Ninja", "cow", "Robot", "littlegirl"};
+    public String randomWord;
+    public static void main(String[] args) {
+    
+        System.out.println("Mängid poomismängu!");
+
+        randomWord = getRandomWord();
+
+        System.out.println("")    
     }
+    
+
+    
+
+    public static String getRandomWord(){
+        Random r = new Random();
+        String word = words[r.nextInt(words.length-1)];
+        return word;
+    }
+
+    
 }
