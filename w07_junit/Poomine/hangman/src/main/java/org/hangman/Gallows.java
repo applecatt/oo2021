15c82abd4fd33110ec40ed00d4465c9f7c228d85
unknown <har0216@tlu.ee>
2021-04-06 22:05:37 +0300

    Kuues tund

--- w07_junit/Poomine/hangman/src/main/java/org/hangman/Gallows.java
+++ w07_junit/Poomine/hangman/src/main/java/org/hangman/Gallows.java
@@ -1,22 +1,75 @@
 package org.hangman;
 
 public class Gallows {
-    public String nextGallow(int wrongGuess){
+    public String gallows(int wrongGuess){
         String image = "";
         switch(wrongGuess) {
             case 0:
-              break;
+                image+="\n";
+                image+="\n";
+                image+="\n";
+                image+="\n";
+                image+="\n";
+                image+="__/"
+                return image;
             case 1:
-              image+="\n";
-              image+="\n";
-              image+="|";
-              return image;
+                image+="\n";
+                image+="\n";
+                image+="\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
             case 2:
-            image+="\n";
-            image+="|";
-            image+="|";
+                image+="\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 3:
+                image+="_\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 4:
+                image+="__\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 5:
+                image+="___\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 6:
+                image+="_____\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 7:
+                image+="_______\n";
+                image+="|      |\n";
+                image+="|\n";
+                image+="|\n";
+                image+="|\n";
+                return image;
+            case 8: 
+                image+="_______\n";
+                image+="|      |\n";
+                image+="|      Q\n";
+                image+="|    --T--\n";
+                image+="|     | |\n";
+                return image;
             default:
-              // code block
+                return "ERROR";
           }
     }
 }
