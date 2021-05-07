b985f47c0fde34843642b0bd7dcfd3413930a7c9
unknown <har0216@tlu.ee>
2021-05-07 16:15:46 +0300

    kt2

--- w13_kontrolltoo2/jooned/src/test/java/org/kt/AppTest.java
+++ w13_kontrolltoo2/jooned/src/test/java/org/kt/AppTest.java
@@ -35,10 +35,10 @@ public class AppTest
     }@Test
     public void checkJoon2GetY()
     {
-        Joon2 joon2 = new Joon2(3, 0);
-        assertTrue(joon2.getY(2)==6);
-        assertTrue(joon2.getY(4)==12);
-        assertTrue(joon2.getY(3)==9);
+        Joon2 joon2 = new Joon2(3, 1);
+        assertTrue(joon2.getY(2)==7);
+        assertTrue(joon2.getY(4)==13);
+        assertTrue(joon2.getY(3)==10);
         Joon2 joon2_2 = new Joon2(3, 2);
         assertTrue(joon2_2.getY(2)==8);
         assertTrue(joon2_2.getY(4)==14);
@@ -47,14 +47,15 @@ public class AppTest
     @Test
     public void checkJoon2ArrayGetY()
     {
-        Joon2 joon2_3 = new Joon2(3, 2);
+        Joon2 joon2 = new Joon2(3, 2);
         int[] x = new int[]{1,2,3,4,5};
-        assertArrayEquals(Arrays.asList(5,8,11,14,17).toArray(), Arrays.asList(joon2_3.getY(x)).toArray());
+        int[] y = new int[]{5,8,11,14,17};
+        assertArrayEquals(y, joon2.getY(x));
     }
     @Test
     public void checkJoonFile(){
-        String line = null;
-        try (Scanner scanner = new Scanner(new File("joon.txt"))){
+        String line = "";
+        try (Scanner scanner = new Scanner(new File("C:/Users/User/Documents/Kool/Kevad 2021/Objektorienteeritud programmeerimine/w13_kontrolltoo2/jooned/src/main/java/org/kt/joon.txt"))){
             while (scanner.hasNextLine()){
                 line += scanner.nextLine();
             }
@@ -65,17 +66,22 @@ public class AppTest
         for (int i = 0; i<4; i++) {
             array[i] = Integer.parseInt(line.split(",")[i]);
         }
+        int a = array[0];
+        int c = array[1];
+        int min = array[2];
+        int max = array[3];
         JoonLiides joonfail;
-        if (array[1]==0){
-            joonfail = new Joon(array[0]);
+        if (c==0){
+            joonfail = new Joon(a);
         } else {
-            joonfail = new Joon2(array[0], array[1]);
+            joonfail = new Joon2(a, c);
         }
-        int[] x = new int[array[3]-array[2]];
-        for (int j = array[2]; j <= array[3]; j++) {
-            x[j] = j;
+        int[] x = new int[max-min];
+        for (int j = 0; j < x.length; j++) {
+            x[j] = j+min;
         }
-        assertArrayEquals(Arrays.asList(joonfail.getY(x)).toArray(), Arrays.asList(56,61,66,71,76,81).toArray());
+        int[] y = new int[]{56,61,66,71,76};
+        assertArrayEquals(y, joonfail.getY(x));
     }
 
 
